5ad1fc488fab7d332be83e9b311e06b9fb9fa05c
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-01 12:40:24 +0800

    add a UserProfile class and for profile content display.

---
+++ src/main/java/com/codeisright/attendance/cache/UserProfile.java
@@ -0,0 +1,163 @@
+package com.codeisright.attendance.cache;
+
+
+import com.codeisright.attendance.data.Student;
+import com.codeisright.attendance.data.Teacher;
+import com.codeisright.attendance.utils.ImageHadler;
+
+import java.awt.*;
+
+public class UserProfile {
+    private String id;
+
+    private String name;
+
+    private int age;
+
+    private String gender;
+
+    private String department;
+
+    private String major;
+
+    private String theclass;
+
+    private String role;
+
+    private ImageHadler image;
+
+    private Image content;
+
+    public UserProfile() {
+        this.id = "";
+        this.name = "";
+        this.age = -1;
+        this.gender = "";
+        this.department = "";
+        this.major = "";
+        this.theclass = "";
+        this.role = "";
+        this.image = new ImageHadler(id);
+        this.content = null;
+    }
+
+    public UserProfile(String id, String name, int age, String gender, String department, String major, String theclass,
+                       String role) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.department = department;
+        this.major = major;
+        this.theclass = theclass;
+        this.role = role;
+        this.image = new ImageHadler(id);
+        this.content = this.image.getImage();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getTheclass() {
+        return theclass;
+    }
+
+    public void setTheclass(String theclass) {
+        this.theclass = theclass;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public ImageHadler getImage() {
+        return image;
+    }
+
+    public void setImage(ImageHadler image) {
+        this.image = image;
+    }
+
+    public void setTeacher(Teacher teacher){
+        this.gender = teacher.getGender();
+        this.age = teacher.getAge();
+        this.department = teacher.getDepartment();
+        this.name = teacher.getName();
+        this.id = teacher.getId();
+        this.role = "teacher";
+        this.image.setId(id);
+        this.content = this.image.getImage();
+    }
+
+    public void setStudent(Student student){
+        this.gender = student.getGender();
+        this.age = student.getAge();
+        this.major = student.getMajor();
+        this.theclass = student.getTheclass();
+        this.name = student.getName();
+        this.id = student.getId();
+        this.role = "student";
+        this.image.setId(id);
+        this.content = this.image.getImage();
+    }
+
+    @Override
+    public String toString() {
+        return "UserProfile{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", department='" + department + '\'' +
+                ", major='" + major + '\'' +
+                ", theclass='" + theclass + '\'' +
+                ", role=" + role +
+                ", image=" + image +
+                '}';
+    }
+}
--- src/main/java/com/codeisright/attendance/utils/ImageUtils.java
+++ src/main/java/com/codeisright/attendance/utils/ImageUtils.java
@@ -1,4 +1,43 @@
 package com.codeisright.attendance.utils;
 
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 public class ImageUtils {
+
+    public static String getBase64Image(byte[] image) {
+        return Base64.getEncoder().encodeToString(image);
+    }
+
+    public static byte[] getImageFromBase64(String base64Image) {
+        return Base64.getDecoder().decode(base64Image);
+    }
+
+    public static String getBase64ImageFromPath(String path) {
+        return getBase64Image(getImageFromPath(path));
+    }
+
+    public static byte[] getImageFromPath(String path) {
+        try {
+            return Files.readAllBytes(Paths.get(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void saveImageFromBase64(String base64Image, String path) {
+        saveImage(getImageFromBase64(base64Image), path);
+    }
+
+    public static void saveImage(byte[] image, String path) {
+        try {
+            Files.write(Paths.get(path), image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
