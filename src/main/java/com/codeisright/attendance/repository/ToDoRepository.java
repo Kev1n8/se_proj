60ad8fbd180c24108f3ccdc120f529c7ab44fc91
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-05 16:12:57 +0800

    goodbye my friends (met in the very beginning of this project)

--- src/main/java/com/codeisright/attendance/repository/ToDoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codeisright.attendance.repository;
-
-import com.codeisright.attendance.data.ToDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ToDoRepository extends JpaRepository<ToDo, Long>{
-
-}
