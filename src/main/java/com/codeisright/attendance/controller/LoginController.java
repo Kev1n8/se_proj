493ce41a6ec717f58f744a409e4445c460243b5d
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-06 16:38:53 +0800

    add more detail to the function

--- src/main/java/com/codeisright/attendance/controller/LoginController.java
+++ src/main/java/com/codeisright/attendance/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.codeisright.attendance.controller;
 
+import com.codeisright.attendance.cache.UserDto;
+import com.codeisright.attendance.data.Student;
 import com.codeisright.attendance.data.Teacher;
+import com.codeisright.attendance.service.StudentService;
 import com.codeisright.attendance.service.TeacherService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,49 +16,76 @@ import org.springframework.security.web.context.HttpSessionSecurityContextReposi
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class LoginController {
-    private AuthenticationManager authenticationManager;
-    private TeacherService teacherService;
+    private final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    private final AuthenticationManager authenticationManager;
+    private final TeacherService teacherService;
+    private final StudentService studentService;
 
     @Autowired
-    public LoginController(AuthenticationManager authenticationManager, TeacherService teacherService) {
+    public LoginController(AuthenticationManager authenticationManager, TeacherService teacherService, StudentService studentService) {
         this.authenticationManager = authenticationManager;
         this.teacherService = teacherService;
+        this.studentService = studentService;
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password, HttpSession session) {
+    public String login(@RequestParam UserDto dto, HttpSession session) {
+        String username = dto.getUsername();
+        String password = dto.getPassword();
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
         try {
-            Authentication auth = authenticationManager.authenticate(token);
+            logger.info("username: " + username + "pairing...");
+            authenticationManager.authenticate(token);
             session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
                     SecurityContextHolder.getContext());
             return "redirect:/hello";
         }catch (Exception e) {
+            logger.info("username: " + username + "pairing failed");
             return "login?error";
         }
     }
 
     @GetMapping("/logout")
     public String logout(HttpSession session) {
-        session.invalidate();
+        try {
+            session.invalidate();
+            logger.info("logout successfully");
+        } catch (Exception e) {
+            logger.info("logout failed");
+            return "logout?error";
+        }
         return "redirect:/login";
     }
 
     @PostMapping("/register/teacher")
-    public Teacher register(@RequestParam Teacher teacher) {
+    public Teacher registerTeacher(@RequestParam Teacher teacher) {
         try {
+            logger.info("registering teacher: " + teacher.getUsername());
             return teacherService.addTeacher(teacher);
         } catch (Exception e) {
+            logger.info("registering teacher failed");
             return null;
         }
     }
 
     @GetMapping("/register/teacher")
-    public String getTeacher(Model model) {
+    public String getRegTeacher(Model model) {
         return "hello, please register";
     }
 
-
+    @PostMapping("/register/student")
+    public Student registerStudent(@RequestBody Student student) {
+        try {
+            logger.info("registering student: " + student.getUsername());
+            return studentService.registerStudent(student);
+        } catch (Exception e) {
+            logger.info("registering student failed");
+            return null;
+        }
+    }
 }
