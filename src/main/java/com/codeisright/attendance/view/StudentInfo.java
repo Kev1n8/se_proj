dc9345ad0bff418718d8dc5f30b5475934fb4669
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-08 10:21:37 +0800

    add userinfo interface avoiding returning unnecessary password

---
+++ src/main/java/com/codeisright/attendance/view/StudentInfo.java
@@ -0,0 +1,10 @@
+package com.codeisright.attendance.view;
+
+public interface StudentInfo {
+    String getId();
+    String getName();
+    int getAge();
+    String getGender();
+    String getMajor();
+    String getDescription();
+}
---
+++ src/main/java/com/codeisright/attendance/view/TeacherInfo.java
@@ -0,0 +1,9 @@
+package com.codeisright.attendance.view;
+
+public interface TeacherInfo {
+    String getId();
+    String getName();
+    int getAge();
+    String getGender();
+    String getDepartment();
+}
