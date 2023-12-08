c03da25cf641d2e60095549c9886fcb03389aff0
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-08 10:25:27 +0800

    delete useless code and add authorization

--- src/main/java/com/codeisright/attendance/security/ApplicationSecurity.java
+++ src/main/java/com/codeisright/attendance/security/ApplicationSecurity.java
@@ -47,28 +47,14 @@ public class ApplicationSecurity extends SecurityConfigurerAdapter<DefaultSecuri
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //TODO: make sure the security and the Controllers working together
         http
-                .csrf(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/register**").permitAll()
+                        .requestMatchers("/logout").permitAll()
+                        .requestMatchers("/usr/{id}/teacher/**").hasRole("TEACHER")
+                        .requestMatchers("/usr/{id}/student/**").hasRole("STUDENT")
                         .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/auth").permitAll()
-//                        .anyRequest().authenticated()
-//                ).httpBasic(Customizer.withDefaults());
-//        http.formLogin(form -> form
-//                .loginPage("/login")
-//                .permitAll()
-//                .defaultSuccessUrl("/hello", true)
-//                .failureUrl("/login?error")
-//        ).logout(logout -> logout
-//                .logoutUrl("/logout")
-//                .logoutSuccessUrl("/bye")
-//        ).authorizeHttpRequests(authorize -> authorize
-//                .requestMatchers("/register", "/login", "/logout").permitAll()
-//                .anyRequest().authenticated()
-//        ).csrf(CsrfConfigurer::disable);
+                ).csrf(CsrfConfigurer::disable);
         return http.build();
     }
 }
