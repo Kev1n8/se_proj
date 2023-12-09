942d9aaaaff73c2dcffb51914a00746656f73c66
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-09 18:41:46 +0800

    change the order of the filter

--- src/main/java/com/codeisright/attendance/security/ApplicationSecurity.java
+++ src/main/java/com/codeisright/attendance/security/ApplicationSecurity.java
@@ -6,13 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
@@ -48,8 +45,8 @@ public class ApplicationSecurity extends SecurityConfigurerAdapter<DefaultSecuri
         //TODO: make sure the security and the Controllers working together
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/register/**").permitAll()
                         .requestMatchers("/login").permitAll()
-                        .requestMatchers("/register**").permitAll()
                         .requestMatchers("/logout").permitAll()
                         .requestMatchers("/usr/{id}/teacher/**").hasRole("TEACHER")
                         .requestMatchers("/usr/{id}/student/**").hasRole("STUDENT")
