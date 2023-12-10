1122e80b510b41fd5db110b19f8217217a3e0468
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-10 16:36:57 +0800

    add a new param secondDelay to indicate the time alive of the QR generated

--- src/main/java/com/codeisright/attendance/utils/QRCodeUtils.java
+++ src/main/java/com/codeisright/attendance/utils/QRCodeUtils.java
@@ -1,29 +1,21 @@
 package com.codeisright.attendance.utils;
 
 import com.google.zxing.*;
-import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.QRCodeReader;
 import com.google.zxing.qrcode.QRCodeWriter;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
 
 public class QRCodeUtils {
 
     // 生成 QR Code
-    public static String generateQRCode(String data) {
+    public static String generateQRCode(String data, Long secondsDelay) {
         try {
-            // 生成随机前缀，前13位为30秒后的时间戳
-            String randomPrefix = String.valueOf(System.currentTimeMillis() + 30000);
+            // 生成随机前缀，前13位为secondsDelay秒后的时间戳
+            String randomPrefix = String.valueOf(System.currentTimeMillis() + secondsDelay * 1000);
             data = randomPrefix + data;
             QRCodeWriter qrCodeWriter = new QRCodeWriter();
             BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, 200, 200);
@@ -36,31 +28,6 @@ public class QRCodeUtils {
         } catch (WriterException | IOException e) {
             e.printStackTrace();
             return null;
-        }
-    }
-
-    // 扫描 QR Code
-    public static String scanQRCode(String imagePath) {
-        try {
-            BufferedImage bufferedImage = ImageIO.read(new File(imagePath));
-            LuminanceSource luminanceSource = new BufferedImageLuminanceSource(bufferedImage);
-            Binarizer binarizer = new HybridBinarizer(luminanceSource);
-            BinaryBitmap binaryBitmap = new BinaryBitmap(binarizer);
-
-            Map<DecodeHintType, Object> hints = new HashMap<>();
-            hints.put(DecodeHintType.CHARACTER_SET, "UTF-8");
-
-            QRCodeReader qrCodeReader = new QRCodeReader();
-            Result result = qrCodeReader.decode(binaryBitmap, hints);
-
-            return result.getText().substring(13);
-        } catch (IOException | NotFoundException e) {
-            e.printStackTrace();
-            return null;
-        } catch (ChecksumException e) {
-            throw new RuntimeException(e);
-        } catch (FormatException e) {
-            throw new RuntimeException(e);
         }
     }
 
