1b76188b489ac7418c208f730a12762940a0987d
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-16 00:50:59 +0800

    去掉无用import

--- src/main/java/com/codeisright/attendance/repository/StudentRepository.java
+++ src/main/java/com/codeisright/attendance/repository/StudentRepository.java
@@ -4,9 +4,6 @@ import com.codeisright.attendance.data.Student;
 import com.codeisright.attendance.view.StudentInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface StudentRepository extends JpaRepository<Student, String>{
-    Student findStudentById(String id);
     StudentInfo findStudentInfoById(String id);
 }
