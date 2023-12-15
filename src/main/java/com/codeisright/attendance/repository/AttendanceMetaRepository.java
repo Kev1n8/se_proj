d14aad216cad9d7454ad8eaf0e077f427d4d7e85
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-15 21:05:34 +0800

    新增 获取最近结束且没有通知过的meta的接口

--- src/main/java/com/codeisright/attendance/repository/AttendanceMetaRepository.java
+++ src/main/java/com/codeisright/attendance/repository/AttendanceMetaRepository.java
@@ -3,10 +3,13 @@ package com.codeisright.attendance.repository;
 import com.codeisright.attendance.data.AttendanceMeta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AttendanceMetaRepository extends JpaRepository<AttendanceMeta, String> {
     AttendanceMeta findFirstByAclass_IdOrderByDeadlineDesc(String classId);
 
     List<AttendanceMeta> findByAclass_Id(String classId);
+
+    AttendanceMeta findFirstByAclass_IdAndNotifiedIsFalseAndDeadlineBefore(String classId, LocalDateTime now);
 }
