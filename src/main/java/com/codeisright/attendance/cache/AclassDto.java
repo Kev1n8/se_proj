be2deefabbc7367a151951e6e126c412a9fc0f33
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-18 15:59:06 +0800

    创建班级所需的必要信息载体

---
+++ src/main/java/com/codeisright/attendance/cache/AclassDto.java
@@ -0,0 +1,33 @@
+package com.codeisright.attendance.cache;
+
+public class AclassDto {
+    private String title;
+
+    private String description;
+
+    private int grade;
+
+    private String courseId;
+
+    private String teacherId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+}
