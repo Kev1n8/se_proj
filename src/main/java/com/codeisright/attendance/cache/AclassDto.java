f526104316ef9194d743d5c47204636013be721f
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-18 17:14:12 +0800

    加入从班级转换到简洁班级信息的的方法

--- src/main/java/com/codeisright/attendance/cache/AclassDto.java
+++ src/main/java/com/codeisright/attendance/cache/AclassDto.java
@@ -1,5 +1,9 @@
 package com.codeisright.attendance.cache;
 
+import com.codeisright.attendance.data.Aclass;
+
+import java.util.List;
+
 public class AclassDto {
     private String title;
 
@@ -10,6 +14,26 @@ public class AclassDto {
     private String courseId;
 
     private String teacherId;
+
+    public AclassDto(Aclass aclass) {
+        this.title = aclass.getTitle();
+        this.description = aclass.getDescription();
+        this.grade = aclass.getGrade();
+        this.courseId = aclass.getCourse().getId();
+        this.teacherId = aclass.getTeacher().getUsername();
+    }
+
+    public static List<AclassDto> Convert(List<Aclass> ls){
+        List<AclassDto> res = new java.util.ArrayList<>();
+        for (Aclass item : ls) {
+            res.add(new AclassDto(item));
+        }
+        return res;
+    }
+
+    public static AclassDto toDto(Aclass item){
+        return new AclassDto(item);
+    }
 
     public String getTitle() {
         return title;
