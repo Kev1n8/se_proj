3b3fbcec969a3236c1366e5cb61d3d6ec0556b05
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-19 13:57:38 +0800

    用于返回带id的班级info

---
+++ src/main/java/com/codeisright/attendance/view/AclassInfo.java
@@ -0,0 +1,10 @@
+package com.codeisright.attendance.view;
+
+public interface AclassInfo {
+    String getId();
+    String getTitle();
+    String getDescription();
+    int getGrade();
+    String getCourseId();
+    String getTeacherId();
+}
