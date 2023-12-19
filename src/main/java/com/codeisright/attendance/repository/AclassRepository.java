6b5d3b99e9d02caa3eab9914d85c0c4fb0043c1d
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2023-12-19 13:58:00 +0800

    新增classinfo相关接口

--- src/main/java/com/codeisright/attendance/repository/AclassRepository.java
+++ src/main/java/com/codeisright/attendance/repository/AclassRepository.java
@@ -1,13 +1,15 @@
 package com.codeisright.attendance.repository;
 
 import com.codeisright.attendance.data.Aclass;
-import com.codeisright.attendance.data.Course;
+import com.codeisright.attendance.view.AclassInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface AclassRepository extends JpaRepository<Aclass, String> {
-
     List<Aclass> findByTeacherId(String teacherId);
 
+    List<AclassInfo> findAclassInfoByTeacherId(String teacherId);
+
+    AclassInfo findAclassInfoById(String id);
 }
